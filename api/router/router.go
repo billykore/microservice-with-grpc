a6ad01dfc25958a3f32a8f356f1b7fa2a6a2daa2
Billy Kore <billyimmcul2010@gmail.com>
2023-01-16 20:00:26 +0700

    feat: add router for account inquiry

--- api/router/router.go
+++ api/router/router.go
@@ -11,6 +11,7 @@ func New(h handler.Handlers) *gin.Engine {
 	v1 := r.Group("/api/v1")
 	{
 		v1.POST("/account/create", h.Customer.AccountCreation)
+		v1.GET("/account/inquiry", h.Customer.AccountInquiry)
 	}
 
 	return r
