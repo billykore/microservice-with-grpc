ab3e2556929c47c61996f0f0d2e2a6b6ab50b67a
Billy Kore <billyimmcul2010@gmail.com>
2023-01-16 20:01:06 +0700

    feat: account inquiry handler

--- api/handler/customer_handler.go
+++ api/handler/customer_handler.go
@@ -37,18 +37,48 @@ func (h *CustomerHandler) AccountCreation(ctx *gin.Context) {
 	grpcRequest := helper.BuildGrpcRequest(body)
 	grpcResponse, err := h.Client.AccountCreation(ctx, grpcRequest)
 	if err != nil {
-		log.Printf("[grpc response] %v", grpcResponse)
+		log.Printf("[customer grpc response] %v", grpcResponse)
 		log.Printf("[handler error] error create account from grpc service. %v", err)
 		ctx.JSON(http.StatusServiceUnavailable, &response.Customer{
 			ResponseCode:    http.StatusServiceUnavailable,
 			ResponseMessage: "Failed create new account",
 			Error:           "Account service unavailable. Please try again later",
 		})
-	} else {
-		log.Printf("[grpc response] %v", grpcResponse)
-		ctx.JSON(http.StatusOK, &response.Customer{
-			ResponseCode:    http.StatusOK,
-			ResponseMessage: grpcResponse.GetMessage(),
-		})
+		return
 	}
+	log.Printf("[customer grpc response] %v", grpcResponse)
+	ctx.JSON(http.StatusOK, &response.Customer{
+		ResponseCode:    http.StatusOK,
+		ResponseMessage: grpcResponse.GetMessage(),
+	})
+}
+
+func (h *CustomerHandler) AccountInquiry(ctx *gin.Context) {
+	accountNumber := ctx.Query("accountNumber")
+	if accountNumber == "" {
+		log.Printf("[handle error] missing query parameter: accountNumber")
+		ctx.JSON(http.StatusBadRequest, response.Customer{
+			ResponseCode:    http.StatusBadRequest,
+			ResponseMessage: "Failed inquiry account",
+			Error:           "Missing query parameter: accountNumber",
+		})
+		return
+	}
+	grpcResponse, err := h.Client.AccountInquiry(ctx, &pb.InquiryRequest{AccountNumber: accountNumber})
+	if err != nil {
+		log.Printf("[customer grpc response] %v", grpcResponse)
+		log.Printf("[handler error] error inquiry account from grpc service. %v", err)
+		ctx.JSON(http.StatusNotFound, &response.Customer{
+			ResponseCode:    http.StatusServiceUnavailable,
+			ResponseMessage: "Failed inquiry account",
+			Error:           "Account not found",
+		})
+		return
+	}
+	log.Printf("[customer grpc response] %v", grpcResponse)
+	ctx.JSON(http.StatusOK, &response.Customer{
+		ResponseCode:    http.StatusOK,
+		ResponseMessage: "Success inquiry account",
+		Data:            grpcResponse,
+	})
 }
