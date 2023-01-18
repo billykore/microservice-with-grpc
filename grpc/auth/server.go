f90176a261b7e2b0245cedb3c25271790cc59ec4
Billy Kore <billyimmcul2010@gmail.com>
2023-01-18 19:58:42 +0700

    chore: Dockerfile for deploying auth grpc service

--- grpc/auth/server.go
+++ grpc/auth/server.go
@@ -42,7 +42,7 @@ func main() {
 	db := database.New(database.Postgres, &database.Config{
 		DatabaseUser:     "postgres",
 		DatabasePassword: "postgres",
-		DatabaseHost:     "localhost",
+		DatabaseHost:     "172.22.0.1",
 		DatabasePort:     "5432",
 		DatabaseName:     "grpc_auth_service",
 	})
